ae7119a5fb74e7f66dee3029b4a955ce3aa891c4
guemin96 <guemin12@naver.copm>
2021-02-05 16:46:45 +0900

    2주차 마지막!

---
+++ 06.T-SQL/SQLQuery3.sql
@@ -0,0 +1,16 @@
+/*
+SELECT memberID,memberName,memberAddress 
+from memberTbl
+WHERE memberID like 'H%';
+ */
+
+ --select 1;
+
+ use BikeStores
+
+ select * from sales.customers;
+
+ select * from production.products;
+
+ --DB 조회
+ EXEC sp_helpdb;
